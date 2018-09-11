9482a833fa5acccfcc45016720d2481543fe2f4e
haphihung <haphihung55@yahoo.com.vn>
2018-09-11 22:29:25 +0700

    Add tests for plugin module

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -1,17 +1,47 @@
+from unittest import mock
+
 import pytest
 
 from meguca import plugin
+from meguca import exceptions
 
+
+@mock.patch('meguca.utils.load_config', return_value='Test')
+@mock.patch('yapsy.PluginManager.PluginManager.collectPlugins')
+@mock.patch('yapsy.PluginManager.PluginManager.activatePluginByName')
 class TestPlugins():
-    def test_load_plugins(self):
-        plugins = plugin.Plugins('tests/plugins_for_testing')
+    def test_load_plugins(self, mocked_activatePluginByName,
+                          mocked_collectPlugins, mocked_load_config):
+        with mock.patch('yapsy.PluginManager.PluginManager.getAllPlugins') as mocked_getAllPlugins:
+            mocked_plg = mock.Mock(plugin_object=mock.Mock(plg_config=None),
+                                   details={'Core': {'ConfigFile': 'Test'}})
+            type(mocked_plg).name = mock.PropertyMock(return_value='Test')
+            mocked_getAllPlugins.return_value = [mocked_plg]
 
-        assert plugins.load_plugins()['Test plugin 1']['TestSection']['TestKey'] == 'TestVal'
+            plugins_ins = plugin.Plugins('')
 
-    def test_get_plugins_by_category(self):
-        plugins = plugin.Plugins('tests/plugins_for_testing')
-        plugins.load_plugins()
+            assert plugins_ins.load_plugins() == {'Test': 'Test'}
 
-        assert plugins.get_plugins('Stat')[0].name == 'Test plugin 1'
+    @mock.patch('yapsy.PluginManager.PluginManager.getPluginsOfCategory', return_value='Test')
+    def test_get_plugins(self, mocked_getPluginsOfCategory, mocked_activatePluginByName,
+                         mocked_collectPlugins, mocked_load_config):
+        plugins_ins = plugin.Plugins('')
+
+        assert plugins_ins.get_plugins('Test') == 'Test'
+        mocked_getPluginsOfCategory.assert_called_with('Test')
+
+
+class TestEntryMethodParam():
+    def test_getitem(self):
+        param = plugin.EntryPointMethodParam({'Test': 'Test'})
+
+        assert param['Test'] == 'Test'
+
+    def test_getitem_on_non_existent_item(self):
+        param = plugin.EntryPointMethodParam({'Test': 'Test'})
+
+        with pytest.raises(exceptions.NotFound):
+            param['Test1']
+
 
 
