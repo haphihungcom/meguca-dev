d47852c620965bf79337e88f48e67ddfb6c74813
haphihung <haphihung55@yahoo.com.vn>
2018-09-11 22:28:16 +0700

    Reorganize utils and utils tests

---
+++ meguca/utils.py
@@ -0,0 +1,17 @@
+import configparser
+
+def load_config(filename):
+    """Get config from an INI file.
+
+    :param filename: Name of config file
+
+    :rtype: A ConfigParser object
+    """
+
+    config = configparser.ConfigParser()
+    config.optionxform = str
+
+    f = open(filename)
+    config.read(filename)
+
+    return config
