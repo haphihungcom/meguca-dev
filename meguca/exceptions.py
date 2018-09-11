84783a88c4cda8c0460f012b2ad0bfb1875a67f1
haphihung <haphihung55@yahoo.com.vn>
2018-09-12 00:14:24 +0700

    Remove ns_api exceptions from meguca's exceptions module

--- meguca/exceptions.py
+++ meguca/exceptions.py
@@ -1,31 +1,14 @@
 class Meguca(Exception):
-    """Base exception for all Meguca-related exceptions"""
+    """Base exception for all Meguca-related exceptions."""
 
 
 class PluginError(Meguca):
-    """Base exception for plugin-related exceptions"""
+    """Base exception for plugin-related exceptions."""
+
+
+class NotYetExist(PluginError):
+    """Raise if get a non-yet-exist item from entry-point method's parameters."""
 
 
 class NotFound(PluginError):
-    """Raise if get a non-existent key from entry-point method's parameters"""
-
-
-class NSAPIError(Meguca):
-    """Base exception for NS API-related exceptions"""
-
-
-class NSAPIRateLimitError(NSAPIError):
-    """Raise if API rate limit was exceeded"""
-
-
-class NSAPIReqError(NSAPIError):
-    """Raise if there is an error in the API request
-    i.e non-existent regions, incorrect shard names, parameters,...
-    """
-
-
-class NSAPIAuthError(NSAPIError):
-    """Raise if there is an error during authentication
-    for private calls
-    i.e incorrect password
-    """
+    """Raise if get a non-existent item from entry-point method's parameters."""
