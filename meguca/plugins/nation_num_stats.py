a9c7ef87a0cc4dcc0f88792c3816525199f53f2f
haphihung <haphihung55@yahoo.com.vn>
2018-09-15 14:21:22 +0700

    Fixes module imports

--- meguca/plugins/nation_num_stats.py
+++ meguca/plugins/nation_num_stats.py
@@ -1,9 +1,9 @@
-from .. import plugin_api as pa
+from meguca import plugin_categories
 
-class WAStats(plugin_category.Stats):
-    def run():
-        wa_nation_num = pa.data['wa_nations'].number_of_nodes()
-        wa_nation_perc = wa_nation_num / pa.data['regional_nation_num'] * 100
+class WAStats(plugin_categories.Stat):
+    def run(self, data):
+        wa_nation_num = data['wa_nations'].number_of_nodes()
+        wa_nation_perc = wa_nation_num / data['regional_nation_num'] * 100
 
         return {'wa_nation_num': wa_nation_num,
                 'wa_nation_perc': wa_nation_perc}
--- meguca/plugins/wa_stats.py
+++ meguca/plugins/wa_stats.py
@@ -1,6 +1,6 @@
-from .. import plugin_category
+from meguca import plugin_categories
 
-class WAStats(plugin_category.Stats):
+class WAStats(plugin_categories.Stat):
     def run(self, data):
         wa_nation_num = data['wa_nations'].number_of_nodes()
         wa_nation_perc = wa_nation_num / data['regional_nation_num'] * 100
