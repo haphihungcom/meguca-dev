effb19d4fcc011f9d8faae2afd0081c35e8bcd21
haphihung <haphihung55@yahoo.com.vn>
2018-10-13 14:35:27 +0700

    Add WAStats plugin tests

---
+++ tests/test_wa_stats.py
@@ -0,0 +1,14 @@
+import networkx as nx
+
+from meguca.plugins import wa_stats
+
+class TestWAStats():
+    def test(self):
+        ins = wa_stats.WAStats()
+        mocked_data = {'wa_nations': nx.DiGraph([('nation1', 'nation2')]),
+                       'regional_nation_num': 2}
+
+        result = ins.run(mocked_data)
+
+        assert result['wa_nation_num'] == 2
+        assert result['wa_nation_perc'] == 100
