1671bd487ea104578c1006478a1970d8705934be
haphihung <haphihung55@yahoo.com.vn>
2018-10-13 14:31:24 +0700

    Adds regional endo stats plugin

---
+++ meguca/plugins/regional_endo_stats.py
@@ -0,0 +1,11 @@
+import networkx as nx
+
+from meguca import plugin_categories
+
+class RegionalEndoStats(plugin_categories.Stat):
+    def run(self, data):
+        total_endocount = data['wa_nations'].number_of_edges()
+        cross_endo = nx.density(data['wa_nations']) * 100
+
+        return {'total_ec': total_endocount,
+                'cross_endo': cross_endo}
---
+++ tests/test_regional_endo_stats.py
@@ -0,0 +1,13 @@
+import networkx as nx
+
+from meguca.plugins import regional_endo_stats
+
+class TestRegionalEndoStats():
+    def test(self):
+        ins = regional_endo_stats.RegionalEndoStats()
+        mocked_data = {'wa_nations': nx.DiGraph([('nation1', 'nation2'), ('nation2', 'nation1')])}
+
+        result = ins.run(mocked_data)
+
+        assert result['total_ec'] == 2
+        assert result['cross_endo'] == 100
