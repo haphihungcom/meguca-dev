1487f64d9dbee685098d6269fb100afdb5b9363e
haphihung <haphihung55@yahoo.com.vn>
2018-10-28 00:53:03 +0700

    Integer conversion

--- meguca/plugins/src/delegate_stats.py
+++ meguca/plugins/src/delegate_stats.py
@@ -2,11 +2,11 @@ from meguca import plugin_categories
 
 
 class DelegateStats(plugin_categories.Stat):
-    def run(self, ns_api, data, config):
-        incumbent_delegate = ns_api.get_region(config['Meguca']['General']['region'], 'delegate')
+    def run(self, ns_api, config):
+        incumbent_delegate = ns_api.get_region(config['Meguca']['General']['region'], 'delegate')['DELEGATE']
         delegate_ec = ns_api.get_nation(self.plg_config['Delegate']['Name'],
-                                     'census',
-                                     {'mode': 'score', 'scale': '66'})['CENSUS']['SCALE']['SCORE']
+                                        'census',
+                                        {'mode': 'score', 'scale': '66'})['CENSUS']['SCALE']['SCORE']
 
         return {'incumbent_delegate': incumbent_delegate,
-                'delegate_ec': total_endocount}
+                'delegate_ec': int(delegate_ec)}
--- tests/test_plugins/test_nation_num_stats.py
+++ tests/test_plugins/test_nation_num_stats.py
@@ -1,18 +1,17 @@
 import configparser
 from unittest import mock
 
-import networkx as nx
-
 from meguca.plugins.src import nation_num_stats
 
-class TestRegionalEndoStats():
+class TestNationNumStats():
     def test(self):
         ins = nation_num_stats.NationNumStats()
-        mocked_nsapi = mock.Mock(get_world=mock.Mock(return_value={'NUMNATIONS': 4}),
-                                 get_region=mock.Mock(return_value={'NUMNATIONS': 2}))
+        mocked_nsapi = mock.Mock(get_world=mock.Mock(return_value={'NUMNATIONS': '4'}),
+                                 get_region=mock.Mock(return_value={'NUMNATIONS': '2'}))
         mocked_config = {'Meguca': configparser.ConfigParser()}
         mocked_config['Meguca']['General'] = {'useragent': '', 'region': 'region'}
 
         result = ins.run(ns_api=mocked_nsapi, config=mocked_config)
 
-        assert result['nation_num'] == 2
+        assert result == {'region_nation_num': 2,
+                          'ns_nation_num': 4}
