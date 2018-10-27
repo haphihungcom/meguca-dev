2b493e3517e7af93ac0a4d53fb0623ba83565e73
haphihung <haphihung55@yahoo.com.vn>
2018-10-27 23:58:30 +0700

    Rewrite test

--- tests/test_plugins/test_nation_num_stats.py
+++ tests/test_plugins/test_nation_num_stats.py
@@ -6,14 +6,13 @@ import networkx as nx
 from meguca.plugins.src import nation_num_stats
 
 class TestRegionalEndoStats():
-    @mock.patch('meguca.plugins.services.ns_api.ns_api.NSApi.get_world', return_value={'NUMNATIONS': 4})
-    @mock.patch('meguca.plugins.services.ns_api.ns_api.NSApi.get_region', return_value={'NUMNATIONS': 2})
-    def test(self, mocked_get_region, mocked_get_world):
+    def test(self):
         ins = nation_num_stats.NationNumStats()
+        mocked_nsapi = mock.Mock(get_world=mock.Mock(return_value={'NUMNATIONS': 4}),
+                                 get_region=mock.Mock(return_value={'NUMNATIONS': 2}))
         mocked_config = {'Meguca': configparser.ConfigParser()}
         mocked_config['Meguca']['General'] = {'useragent': '', 'region': 'region'}
 
-        result = ins.run(mocked_config)
+        result = ins.run(ns_api=mocked_nsapi, config=mocked_config)
 
         assert result['nation_num'] == 2
-        assert result['nation_num_perc'] == 50
