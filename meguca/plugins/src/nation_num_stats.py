218d33f462708aeb80823c1be1ba8a81b0c328b3
haphihung <haphihung55@yahoo.com.vn>
2018-10-28 00:53:36 +0700

    Add integer conversion

--- meguca/plugins/src/nation_num_stats.py
+++ meguca/plugins/src/nation_num_stats.py
@@ -1,8 +1,10 @@
 from meguca import plugin_categories
 
+
 class NationNumStats(plugin_categories.Stat):
     def run(self, ns_api, config):
-        nation_num = ns_api.get_region(config['Meguca']['General']['region'], 'numnations')['NUMNATIONS']
+        region_nation_num = ns_api.get_region(config['Meguca']['General']['region'], 'numnations')['NUMNATIONS']
         ns_nation_num = ns_api.get_world('numnations')['NUMNATIONS']
 
-        return {'nation_num': nation_num}
+        return {'region_nation_num': int(region_nation_num),
+                'ns_nation_num': int(ns_nation_num)}
