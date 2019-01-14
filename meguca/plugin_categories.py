7f243b700bede8eeff43eba73518f0f4a32ad8d8
Ha Phi Hung <haphihung55@gmail.com>
2019-01-14 23:24:43 +0700

    Fix docstring wording, make prime run implementation optional

--- meguca/plugin_categories.py
+++ meguca/plugin_categories.py
@@ -15,7 +15,7 @@ class Service(MegucaPlugin):
     """
 
     def get(self):
-        """Return service objects."""
+        """Return an instance of the service."""
 
         raise NotImplementedError
 
@@ -24,35 +24,33 @@ class StandardPlugin(MegucaPlugin):
     """Base class for standard plugins."""
 
     def run(self):
-        """Entry point method of all standard plugins."""
+        """Entry method."""
 
         raise NotImplementedError
 
 
 class Collector(StandardPlugin):
     """Base class for plugins that collect data.
-    Implement run() as entry point for normal run mode
-    and prime_run() as entry point for prime run mode.
+    Implement run() as entry point for normal run
+    and prime_run() as entry point for prime run (optional).
     run() and prime_run() needs to return a dict which contains
     data you want to return. The key names in the dict will be used
-    as identifiers of the data you return. These identifiers
+    as identifiers for the data you return. These identifiers
     will be used by Meguca and other plugins to get the data
     created by this plugin.
     """
 
     def prime_run(self):
-        """Entry point method for prime run."""
-
-        raise NotImplementedError
+        """Entry method for prime run."""
 
 
 class Stat(StandardPlugin):
-    """Base class for plugins that calculate and return stats
+    """Base class for plugins that calculate and return data
     Implement run() as entry point.
     run() needs to return a dict contains stats you want to
     return. The dict's key names will be used as identifiers
     of the stats you return. These identifiers will be used by
-    Meguca and other plugins to access the stats created
+    Meguca and other plugins to get the data created
     by this plugin.
     """
 
