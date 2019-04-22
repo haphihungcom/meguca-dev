c1cb22a7faa10341d2418badaa03fc0c86a91e3f
Ha Phi Hung <haphihung55@gmail.com>
2019-04-22 11:19:59 +0700

    List generate filter

--- meguca/plugins/src/dispatch_updater/filters.py
+++ meguca/plugins/src/dispatch_updater/filters.py
@@ -29,3 +29,29 @@ def region(name):
     """
 
     return '[region]{}[/region]'.format(name)
+
+
+def gen_list(input_list, delimiter=', ', tag='nation', tag_modifier=None):
+    """Generate a text list with each item separated by a delimiter.
+    E.g [nation]nation1[/nation], [nation]nation2[/nation], [nation]nation3[/nation]
+
+    Args:
+        input_list (list): List.
+        delimiter (str, optional): Delimiter. Defaults to ', '.
+        tag (str, optional): Tag to wrap around an item. Defaults to 'nation'.
+        tag_modifier (str, optional): Tag modifier. Defaults to None.
+
+    Returns:
+        str: A text list.
+    """
+
+    result = ""
+
+    if tag is None:
+            result += delimiter.join(input_list)
+    elif tag_modifier is None:
+        result = delimiter.join(["[{}]{}[/{}]".format(tag, item, tag) for item in input_list])
+    else:
+        result = delimiter.join(["[{}={}]{}[/{}]".format(tag, tag_modifier, item, tag) for item in input_list])
+
+    return result
--- tests/test_plugins/test_dispatch_updater/test_filters.py
+++ tests/test_plugins/test_dispatch_updater/test_filters.py
@@ -14,3 +14,14 @@ class TestNation():
 class TestRegion():
     def test_default(self):
         assert filters.region('test') == '[region]test[/region]'
+
+
+class TestGenList():
+    def test_no_tag(self):
+        assert filters.gen_list(['1', '2', '3'], tag=None) == '1, 2, 3'
+
+    def test_with_tag_no_param(self):
+        assert filters.gen_list(['1', '2', '3'], tag='a') == '[a]1[/a], [a]2[/a], [a]3[/a]'
+
+    def test_with_tag_and_param(self):
+        assert filters.gen_list(['1', '2', '3'], tag='a', tag_modifier='1') == '[a=1]1[/a], [a=1]2[/a], [a=1]3[/a]'
